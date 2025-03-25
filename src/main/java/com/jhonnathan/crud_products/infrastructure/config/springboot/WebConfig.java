d4774737f1e3a0a6bdfef87141086cd419b88f00
Jonathan Urrego <jadougdev@gmail.com>
2025-03-25 11:35:14 -0500

    cors

---
+++ src/main/java/com/jhonnathan/crud_products/infrastructure/config/springboot/WebConfig.java
@@ -0,0 +1,24 @@
+package com.jhonnathan.crud_products.infrastructure.config.springboot;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:5173") // Cambia esto si tienes un dominio en producción
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
