a19d45a90aa857ee76105e89928f1d445c24b191
Jonathan Urrego <jadougdev@gmail.com>
2025-03-25 12:50:55 -0500

    .

--- src/main/java/com/jhonnathan/crud_products/infrastructure/config/springboot/WebConfig.java
+++ src/main/java/com/jhonnathan/crud_products/infrastructure/config/springboot/WebConfig.java
@@ -3,9 +3,11 @@ package com.jhonnathan.crud_products.infrastructure.config.springboot;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class WebConfig {
 
     @Bean
@@ -13,9 +15,9 @@ public class WebConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173") // Cambia esto si tienes un dominio en producción
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                registry.addMapping("/")
+                        .allowedOrigins("*")
+                        .allowedMethods("*")
                         .allowedHeaders("*")
                         .allowCredentials(true);
             }
--- src/main/java/com/jhonnathan/crud_products/infrastructure/controller/ProductsController.java
+++ src/main/java/com/jhonnathan/crud_products/infrastructure/controller/ProductsController.java
@@ -12,7 +12,6 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/products")
 public class ProductsController {
