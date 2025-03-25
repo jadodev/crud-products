06eec7b53fc8a43bbdcd1b8ec2249371c8dd407d
Jonathan <jadougdev@gmail.com>
2025-03-25 12:40:25 -0500

    Update ProductsController.java

--- src/main/java/com/jhonnathan/crud_products/infrastructure/controller/ProductsController.java
+++ src/main/java/com/jhonnathan/crud_products/infrastructure/controller/ProductsController.java
@@ -12,7 +12,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "http://localhost:5174")
 @RestController
 @RequestMapping("/products")
 public class ProductsController {
