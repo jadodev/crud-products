88e700fd78fe1e5d2511957ecac49c2c46b2d153
Jonathan <jadougdev@gmail.com>
2025-03-25 12:40:03 -0500

    Update WebConfig.java

--- src/main/java/com/jhonnathan/crud_products/infrastructure/config/springboot/WebConfig.java
+++ src/main/java/com/jhonnathan/crud_products/infrastructure/config/springboot/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173") // Cambia esto si tienes un dominio en producción
+                        .allowedOrigins("*") // Cambia esto si tienes un dominio en producción
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
